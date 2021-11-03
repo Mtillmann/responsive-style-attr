2f39094b0db743e908723c7414178af0f33a01cb
martin <mtillmann@gmail.com>
2021-11-04 00:22:49 +0100

    added gh pages

--- src/debugMessage.ts
+++
@@ -1,7 +0,0 @@
-import {defaultOptions} from "./defaultOptions";
-
-export function emitDebugMessage(data: {}, type = 'info') {
-    if (defaultOptions.debug) {
-        (console as any)[type]('ResponsiveStyleAttributes DEBUG:', data);
-    }
-}
